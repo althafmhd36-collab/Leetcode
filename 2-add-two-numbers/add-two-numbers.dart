db87ac7b754780e7efc29b9800112a2936a8860b
althafmhd36-collab <althafmhd36@gmail.com>
2026-02-24 20:56:41 +0530

    Time: 1 ms (100.00%) | Memory: 149.9 MB (64.26%) - LeetSync

---
+++ 2-add-two-numbers/add-two-numbers.dart
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *   int val;
+ *   ListNode? next;
+ *   ListNode([this.val = 0, this.next]);
+ * }
+ */
+class Solution {
+  ListNode? addTwoNumbers(ListNode? l1, ListNode? l2) {
+    ListNode dummyHead = ListNode(0);
+    ListNode current = dummyHead;
+    int carry = 0;
+
+    while (l1 != null || l2 != null || carry != 0) {
+      int x = (l1 != null) ? l1.val : 0;
+      int y = (l2 != null) ? l2.val : 0;
+      int sum = x + y + carry;
+
+      carry = sum ~/ 10;
+      current.next = ListNode(sum % 10);
+      current = current.next!;
+
+      if (l1 != null) l1 = l1.next;
+      if (l2 != null) l2 = l2.next;
+    }
+
+    return dummyHead.next;
+  }
+}
