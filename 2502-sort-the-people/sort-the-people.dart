5548324d490937938b8616b6b77c69eef6ab9990
althafmhd36-collab <althafmhd36@gmail.com>
2026-04-24 16:02:27 +0530

    Time: 26 ms (66.67%) | Memory: 158.4 MB (16.67%) - LeetSync

---
+++ 2502-sort-the-people/sort-the-people.dart
@@ -0,0 +1,19 @@
+class Solution {
+  List<String> sortPeople(List<String> names, List<int> heights) {
+    
+    List<Map <String , dynamic>> people = [];
+
+    for ( int i=0; i < names.length; i++){
+        people.add({
+             'name' : names[i],
+             'height': heights[i]
+        });
+
+    }
+
+    people.sort((a ,b ) =>  b['height'].compareTo(a['height']) );
+
+
+    return people.map((person) => person['name'] as String  ).toList();
+  }
+}
